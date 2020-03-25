a478519c6055e61597aeb764327698ef9f9d62a6
Hardik Prajapati <42292587+HardikPrajapati1998@users.noreply.github.com>
2020-03-25 22:47:18 +0530

    uploading more than one at a time.
    uploading more than one at a time.

--- header.php
+++ header.php
@@ -84,7 +84,7 @@ $result = $user->con_details($sql);
 
       <div class="input-group mb-3">
   <div class="custom-file">
-    <input type="file" class="custom-file-input" name="fileToUpload" id="inputGroupFile02">
+    <input type="file" class="custom-file-input" name="fileToUpload[]" id="inputGroupFile02" multiple="true">
     <label class="custom-file-label" for="inputGroupFile02">Choose file</label>
   </div>
   <div class="input-group-append">
