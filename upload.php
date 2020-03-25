48f60b0918991508da09b60f37cfb57c8b2ad321
Hardik Prajapati <42292587+HardikPrajapati1998@users.noreply.github.com>
2020-03-25 22:50:18 +0530

    uploading more than one at a time.
    uploading more than one at a time.

--- upload.php
+++ upload.php
@@ -7,16 +7,16 @@ include_once 'Conn.php';
     $user = new User();
      
     if(isset($_POST['submit'])){
-       
+       for ($i = 0; $i < count($_FILES['fileToUpload']['name']); $i++) {
             $target_dir = "uploads/";
-            $imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"]),PATHINFO_EXTENSION));
+            $imageFileType = strtolower(pathinfo(basename($_FILES["fileToUpload"]["name"][$i]),PATHINFO_EXTENSION));
             $newfilename = time().'_phpproject.'.$imageFileType ;
             $target_file = $target_dir . $newfilename ;
             
             $uploadOk = 1;
             // Check if image file is a actual image or fake image
             if(isset($_POST["submit"])) {
-                $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+                $check = getimagesize($_FILES["fileToUpload"]["tmp_name"][$i]);
                 if($check !== false) {
                     echo "File is an image - " . $check["mime"] . ".";
                     $uploadOk = 1;
@@ -35,7 +35,7 @@ include_once 'Conn.php';
                 $uploadOk = 0;
             }*/
             // Check file size
-            if ($_FILES["fileToUpload"]["size"] > 5000000) {
+            if ($_FILES["fileToUpload"]["size"][$i] > 5000000) {
                 echo "<script>
                 alert('Sorry, your file is too large.');
                  window.location.href='gallery.php';
@@ -62,7 +62,7 @@ include_once 'Conn.php';
                  </script>"; 
             // if everything is ok, try to upload file
             } else {
-                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"][$i], $target_file)) {
                     echo "<script>
                 alert(' successful file upload');
                  window.location.href='gallery.php';
@@ -79,7 +79,7 @@ include_once 'Conn.php';
                 }
             }
             
-
+       }
         }
 
 ?>
